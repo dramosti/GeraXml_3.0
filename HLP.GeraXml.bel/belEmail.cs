7d061a05f3683c08ba7767071c72d133a4407201
dramosti <diego@hlp.com.br>
2012-12-07 13:55:21 -0200

    Diego - teste de commit

--- HLP.GeraXml.bel/belEmail.cs
+++ HLP.GeraXml.bel/belEmail.cs
@@ -69,6 +69,7 @@ namespace HLP.GeraXml.bel
                     {
                         this.sOutros += aux[i].Trim();
                     }
+
                 }
             }
             else
